33e924a504603e816914160d98c76ab98164c5e8
0918nobita <nobita.0918@gmail.com>
2021-02-19 03:29:42 +0900

    :construction: 写経を進めた

--- main.cpp
+++ main.cpp
@@ -26,5 +26,21 @@ static bool parseOptions(int argc, char** argv) {
         ("noprompt,n", "suppress prompt.")
         ("nook,k",     "suppress 'ok'.")
         ("time,k",     "display spent time.");
+
+    po::variables_map vm;
+    std::vector<std::string> argVec;
+
+    try {
+        auto const parseResult = po::parse_command_line(argc, argv, description);
+        po::store(parseResult, vm);
+        po::notify(vm);
+
+        for (auto const& str : po::collect_unrecognized(parseResult.options, po::include_positional)) {
+            argVec.push_back(str);
+        }
+    } catch(const po::error& e) {
+        std::cerr << e.what() << std::endl << description << std::endl;
+    }
+
     return true;
 }
