f2d9305493d5295dc17b8e213217edfe263fa747
0918nobita <nobita.0918@gmail.com>
2021-02-19 14:39:18 +0900

    :construction: help, version フラグに対する処理を追加

--- main.cpp
+++ main.cpp
@@ -1,20 +1,51 @@
+#define VERSION "0.94.0"
+
 #include <iostream>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
-static bool parseOptions(int argc, char** argv);
+const char *kVersion = VERSION;
 
-int main(int argc, char** argv) {
-    if (parseOptions(argc, argv) == false) {
-        return EXIT_FAILURE;
+enum class Command {
+    PrintUsage,
+    PrintVersion,
+    Proceed,
+    Failure,
+};
+
+static Command parseOptions(int argc, char* argv[], std::vector<std::string>& args);
+
+static void printUsage();
+static void printVersion();
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> args;
+    auto command = parseOptions(argc, argv, args);
+
+    switch (command) {
+        case Command::Failure:
+            return EXIT_FAILURE;
+
+        case Command::PrintUsage:
+            printUsage();
+            break;
+
+        case Command::PrintVersion:
+            printVersion();
+            break;
+
+        case Command::Proceed:
+            for (size_t i = 0; i < args.size(); i++) {
+                std::cout << args[i] << std::endl;
+            }
     }
 
     return EXIT_SUCCESS;
 }
 
 /** コマンドライン引数をパースする */
-static bool parseOptions(int argc, char** argv) {
+static Command parseOptions(int argc, char* argv[], std::vector<std::string>& args) {
     po::options_description description("options");
     description.add_options()
         ("help,h",    "print help.")
@@ -28,7 +59,6 @@ static bool parseOptions(int argc, char** argv) {
         ("time,k",     "display spent time.");
 
     po::variables_map vm;
-    std::vector<std::string> argVec;
 
     try {
         auto const parseResult = po::parse_command_line(argc, argv, description);
@@ -36,11 +66,23 @@ static bool parseOptions(int argc, char** argv) {
         po::notify(vm);
 
         for (auto const& str : po::collect_unrecognized(parseResult.options, po::include_positional)) {
-            argVec.push_back(str);
+            args.push_back(str);
         }
     } catch(const po::error& e) {
         std::cerr << e.what() << std::endl << description << std::endl;
+        return Command::Failure;
     }
 
-    return true;
+    if (vm.count("help")) return Command::PrintUsage;
+    if (vm.count("version")) return Command::PrintVersion;
+
+    return Command::Proceed;
+}
+
+static void printUsage() {
+    std::cout << "Usage: para [-ehknqtv] [--thread] [program-file] [program-code]" << std::endl;
+}
+
+static void printVersion() {
+    std::cout << "Paraphrase " << kVersion << std::endl;
 }
