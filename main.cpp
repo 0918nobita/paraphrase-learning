62b22c344ce6eec9cbaea0001bf95ae851e50009
0918nobita <nobita.0918@gmail.com>
2021-02-20 13:26:31 +0900

    :wrench: clang-format を使ってコードをフォーマットするようにした

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #define VERSION "0.94.0"
 
-#include <iostream>
 #include <boost/program_options.hpp>
+#include <iostream>
 
 namespace po = boost::program_options;
 
-const char *kVersion = VERSION;
+const char* kVersion = VERSION;
 
 struct Configuration {
     bool displayOk = true;
@@ -61,28 +61,32 @@ int main(int argc, char* argv[]) {
 /** コマンドライン引数をパースする */
 static void parseOptions(int argc, char* argv[], ParseResult& result) noexcept {
     po::options_description description("options");
+    // clang-format off
     description.add_options()
         ("help,h",    "print help.")
         ("version,v", "print version info.")
         ("quiet,q",   "quiet mode (equivalent to -nk).")
-        ("thread",          po::value<int>(),         "set maximum thread (ex: --thread 8).")
+        ("thread",          po::value<int>(), "set maximum thread (ex: --thread 8).")
         ("eval,e",          po::value<std::string>(), "eval parameter.")
         ("eval-and-exit,E", po::value<std::string>(), "eval and exit (for one liner).")
         ("noprompt,n", "suppress prompt.")
-        ("nook,k",     "suppress 'ok'.")
-        ("time,k",     "display spent time.");
+        ("nook,k", "suppress 'ok'.")
+        ("time,k", "display spent time.");
+    // clang-format on
 
     po::variables_map vm;
 
     try {
-        auto const parseResult = po::parse_command_line(argc, argv, description);
+        auto const parseResult =
+            po::parse_command_line(argc, argv, description);
         po::store(parseResult, vm);
         po::notify(vm);
 
-        for (auto const& str : po::collect_unrecognized(parseResult.options, po::include_positional)) {
+        for (auto const& str : po::collect_unrecognized(
+                 parseResult.options, po::include_positional)) {
             result.args.push_back(str);
         }
-    } catch(const po::error& e) {
+    } catch (const po::error& e) {
         std::cerr << e.what() << std::endl << description << std::endl;
         std::exit(EXIT_FAILURE);
     }
@@ -100,16 +104,20 @@ static void parseOptions(int argc, char* argv[], ParseResult& result) noexcept {
     if (vm.count("time")) result.config.displayTime = true;
     if (vm.count("noprompt")) result.config.usePrompt = false;
     if (vm.count("nook")) result.config.displayOk = false;
-    if (vm.count("quiet")) result.config.usePrompt = result.config.displayOk = false;
+    if (vm.count("quiet"))
+        result.config.usePrompt = result.config.displayOk = false;
     if (vm.count("thread")) {
         auto n = vm["thread"].as<int>();
         if (n < 0) {
-            std::cerr << "The value of --thread argument should be a positive integer" << std::endl;
+            std::cerr
+                << "The value of --thread argument should be a positive integer"
+                << std::endl;
             std::exit(EXIT_FAILURE);
         }
         result.config.numCores = n;
     }
-    if (vm.count("eval")) result.config.strToEval = vm["eval"].as<std::string>();
+    if (vm.count("eval"))
+        result.config.strToEval = vm["eval"].as<std::string>();
     if (vm.count("eval-and-exit")) {
         result.config.strToEval = vm["eval-and-exit"].as<std::string>();
         result.config.evalAndExit = true;
@@ -117,7 +125,9 @@ static void parseOptions(int argc, char* argv[], ParseResult& result) noexcept {
 }
 
 static void printUsage() noexcept {
-    std::cout << "Usage: para [-ehknqtv] [--thread] [program-file] [program-code]" << std::endl;
+    std::cout
+        << "Usage: para [-ehknqtv] [--thread] [program-file] [program-code]"
+        << std::endl;
 }
 
 static void printVersion() noexcept {
